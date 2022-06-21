018b70df38c1049988f9e80166b939eded02e732
Asmawi137 <nanawithlove40@gmail.com>
2022-06-22 00:43:24 +0800

    commit 2 asmawi

--- app/src/main/java/com/example/ligachampion/ListClubAdapter.java
+++ app/src/main/java/com/example/ligachampion/ListClubAdapter.java
@@ -1,8 +1,68 @@
 package com.example.ligachampion;
 
 
+import android.graphics.drawable.Drawable;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.behavior.SwipeDismissBehavior;
 
-public class ListClubAdapter extends RecycerView.Adapter<ListClubAdapter.listViewHolder> {
+import org.w3c.dom.Text;
+
+import java.security.AccessController;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListClubAdapter extends RecyclerView.Adapter<ListClubAdapter.ListViewHolder {
+    private ArrayList<Club> listClub;
+
+    public ListClubAdapter(ArrayList<Club> list) {
+        this.listClub = list;
+    }
+    @NonNull
+    @Override
+    public ListClubAdapter.ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_club,parent, false)
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder (@NonNull ListViewHolder holder, int position) {
+        Club club = listClub.get(position);
+        Glide.with(holder.itemView.getContext()) RequestManager
+            .load(club.getPhoto())RequestBuilder<Drawable>
+            .apply(new RequestOptions().override( width; 50, height; 50))
+            .into(holder.imgPhoto);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return  listClub.size();
+    }
+
+    class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        TextView tvName;
+        TextView tvDetail;
+
+        ListViewHolder(View itemview) {
+            super(itemview);
+            imgPhoto = itemview.findViewById(R.id.img_item_club);
+            tvName = itemview.findViewById(R.id.tv_item_name);
+            tvDetail = itemview.findViewById(R.id.tv_detail);
+        }
+    }
 }
--- app/src/main/java/com/example/ligachampion/MainActivity.java
+++ app/src/main/java/com/example/ligachampion/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.ligachampion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvClub;
+    private ArrayList<Club> list = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvClub = findViewById(R.id.rv_club);
+        rvClub.setHasFixedSize(true);
+
+        list.addAll(ClubData.getlisData());
+    }
+
+    private void showRecyclerList(){
+        rvClub.setLayoutManager(new LinearLayoutManager(this));
+        ListClubAdapter listClubAdapter = new ListClubAdapter(list);
+        rvClub.setAdapter(listClubAdapter);
+
     }
 }
